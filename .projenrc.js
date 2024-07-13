913ea41a64c60db20a229873e9b37025269d0b01
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-07-13 17:37:02 +0300

    chore: remove express dependency

--- .projenrc.js
+++ .projenrc.js
@@ -6,7 +6,7 @@ const project = new javascript.NodeProject({
   authorName: author,
   defaultReleaseBranch: 'main',
   name: 'kl-function-nodejs',
-  deps: ['express'] /* Runtime dependencies of this module. */,
+  deps: [],
   description: 'KubeLambda JavaScript function',
   prettier: true,
   prettierOptions: {
