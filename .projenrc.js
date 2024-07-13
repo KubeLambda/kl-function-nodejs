322c46efee05c914bf77456dfd00ac0349dd3cf3
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-07-13 15:33:01 +0300

    feat: main project configuration

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,23 @@
-const { javascript } = require("projen");
-const project = new javascript.NodeProject({
-  authorName: "Bohdan Frankovskyi",
-  defaultReleaseBranch: "main",
-  name: "kl-function-nodejs",
+const { TrailingComma } = require('projen/lib/javascript')
+const { javascript } = require('projen')
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+const author = 'Bohdan Frankovskyi'
+const project = new javascript.NodeProject({
+  authorName: author,
+  defaultReleaseBranch: 'main',
+  name: 'kl-function-nodejs',
+  deps: ['express'] /* Runtime dependencies of this module. */,
+  description: 'KubeLambda JavaScript function',
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 100,
+      semi: false,
+      singleQuote: true,
+      trailingComma: TrailingComma.ALL,
+    },
+  },
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
-});
-project.synth();
+})
+project.synth()
